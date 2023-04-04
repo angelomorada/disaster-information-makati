ec1c4c48252e17e415f7f9308f512700e24bbba8
Angelo Morada <angelomorada@gmail.com>
2023-04-04 15:09:43 +0800

    created new and create actions for comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,37 @@
 class CommentsController < ApplicationController
   before_action :set_post
+  before_action :set_comment, only: [:edit, :update]
 
   def index
     @comments = @post.comments
+  end
+
+  def new
+    @comment = @post.comments.build
+  end
+
+  def create
+    @comment = post.comments.build(comment_params)
+    if @comment.save
+      redirect_to post_comments_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+
   end
 
   private
 
   def set_post
     @post = Post.find params[:post_id]
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
